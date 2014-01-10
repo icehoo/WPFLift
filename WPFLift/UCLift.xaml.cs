c43b5e678197f971a18c72d0283b6537173a58ab
ZoeCheck <893622396@qq.com>
2014-01-10 12:12:41 +0800

    CreateProject WPFLift

---
+++ WPFLift/UCLift.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFLift
+{
+	/// <summary>
+	/// 作者：xxh 
+	/// 时间：2014-01-10 12:10:14
+	/// 版本：V1.0.0 	 
+	/// </summary>
+	public partial class UCLift : UserControl
+	{
+		public UCLift()
+		{
+			InitializeComponent();
+		}
+	}
+}
